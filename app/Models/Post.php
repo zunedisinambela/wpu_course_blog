689442917681b6ef2bb843f399aff1beca65c9b0
asus <zunedisinambela@gmail.com>
2026-03-23 16:47:14 +0700

    tinker and mass assignments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+  // boleh di isi
+  protected $fillable = ['title', 'author', 'slug', 'body'];
+
+  // tidak boleh di isi
+  // protected $guarded = ['id'];
 }
